ba3cc42425814231965820ffb7865394fc02bf64
gohovd <goran.hovde@gmail.com>
2018-03-06 13:23:06 +0100

    Blog-component (html, css) complete

--- src/app/blog/blog.component.ts
+++ src/app/blog/blog.component.ts
@@ -1,15 +1,22 @@
 import { Component, OnInit } from '@angular/core';
 
 @Component({
-  selector: 'app-blog',
-  templateUrl: './blog.component.html',
-  styleUrls: ['./blog.component.css']
+	selector: 'app-blog',
+	templateUrl: './blog.component.html',
+	styleUrls: ['./blog.component.css']
 })
 export class BlogComponent implements OnInit {
 
-  constructor() { }
+	title = "How to Cut Cheese and Vegetables Like a God";
+	author = "Goran H. Stronstad";
+	day = 6;
+	month = "March";
+	longText = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam quis urna eros. Donec pulvinar, tellus id pretium consectetur, urna arcu placerat risus, id tincidunt nibh est a justo. Nulla volutpat a tellus sed ornare. In massa nibh, euismod non hendrerit ut, consequat a velit. Morbi ultrices efficitur felis, sed facilisis quam aliquam nec. Etiam at ex pharetra nisl dapibus imperdiet.";
+	shortText = "Lorem ipsum dolor sit amet, consectetur adisciping elit.";
 
-  ngOnInit() {
-  }
+	constructor() { }
+
+	ngOnInit() {
+	}
 
 }
